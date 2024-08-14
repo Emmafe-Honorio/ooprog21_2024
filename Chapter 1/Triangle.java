7dcb9f65623e5676e5be022373b0779374d45269
Emmafe-Honorio <emmafehonorio@gmail.com>
2024-08-14 21:38:21 +0800

    Create Triangle.java

---
+++ Chapter 1/Triangle.java
@@ -0,0 +1,12 @@
+class Triangle {
+    public static void main(String[] args) {
+            System.out.println("        l        ");
+            System.out.println("       lll      ");
+            System.out.println("      lllll     ");
+            System.out.println("     lllllll    ");
+            System.out.println("    lllllllll   ");
+            System.out.println("   lllllllllll  ");
+            System.out.println("  lllllllllllll ");
+        }
+    
+}
