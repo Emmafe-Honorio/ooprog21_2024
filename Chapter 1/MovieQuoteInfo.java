4f021947127cd26d8aeaaaf880722c3308f29b87
Emmafe-Honorio <emmafehonorio@gmail.com>
2024-08-14 21:20:32 +0800

    Update MovieQuoteInfo.java

--- Chapter 1/MovieQuoteInfo.java
+++ Chapter 1/MovieQuoteInfo.java
@@ -1,13 +1,9 @@
-public class Main
-{
-	public static void main(String[] args) {
-		System.out.println("Movie Quote:  Happiness can be found even in the darkest of times if only one remembers to turn on the light. ");
-         System.out.println("Movie: Harry Potter and The Prisoner of Azkaban ");
-          System.out.println("Character:  Albus Dumbledore ");
-           System.out.println("Year of the Movie: 2004 ");
+class MovieQuoteInfo {
 
-
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Movie Quote: Happiness can be found even in the darkest of times if only one remembers to turn on the light.");
+        System.out.println("Movie: Harry Potter and the Prisoner of Azkaban");
+        System.out.println("Character: Albus Dumbledore");
+        System.out.println("Year of the Movie: 2004");
+    }
 }
-  
