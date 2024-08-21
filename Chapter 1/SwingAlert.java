a932e51a5da96322f9f922b4e43a451374b75faa
Emmafe-Honorio <emmafehonorio@gmail.com>
2024-08-21 20:56:10 +0800

    Create SwingAlert.java

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,13 @@
+import 
+javax.swing.JOptionPane;
+
+public class SwingAlert{
+    public static void 
+    main(String[] args ){
+        String message = 
+    String.format("Hello!, My name is Emmafe and you can call me by my nickname which is Emma<3");
+    
+    
+    JOptionPane.showMessageDialog(null,message);
+    }
+}
