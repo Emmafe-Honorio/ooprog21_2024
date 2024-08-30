1fa618ec0218f1f64d30eec0665a8a0e65868bdc
Emmafe-Honorio <emmafehonorio@gmail.com>
2024-08-30 07:55:53 +0700

    warmup_sleepin.java

---
+++ Chapter 1/warmup_sleepin.java
@@ -0,0 +1,25 @@
+public class warmup_sleepin {
+    public static boolean
+
+  warmup_sleepin(boolean weekday, boolean
+vacation) {
+
+
+
+return !weekday || vacation;
+}
+
+public static void
+
+main (String[]args){
+
+
+
+System.out.println(warmup_sleepin(false ,false)); //true
+
+System.out.println(warmup_sleepin(true ,false));//false
+
+System.out.println(warmup_sleepin(false ,true));//true
+ }
+
+ }
