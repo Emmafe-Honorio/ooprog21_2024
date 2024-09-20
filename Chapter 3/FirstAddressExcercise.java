579e216619c2c1adc456e07ea4d0ed69cf6444d7
Emmafe-Honorio <emmafehonorio@gmail.com>
2024-09-20 08:59:11 +0700

    Create FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,13 @@
+class FirstAddressExcercise {
+    public static void main(String[] args) {
+        displayAddress();
+        
+    }
+    static void displayAddress(){
+        System.out.println("Apple Park");
+        System.out.println("One Apple Park Way");
+        System.out.println("Cupertino, CA 95014");
+        System.out.println("USA");
+        System.out.println("Company name: Apple Inc.");
+    }
+}
